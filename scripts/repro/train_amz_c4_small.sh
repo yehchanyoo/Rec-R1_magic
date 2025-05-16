d85d70e92ffc4cd1ce49588654fb7dca95717075
Thomas Lilly <thomaslilly65@gmail.com>
2025-05-15 21:52:44 -0700

    Update HF directory to be inside the container

--- scripts/repro/train_amz_c4_small.sh
+++ scripts/repro/train_amz_c4_small.sh
@@ -4,7 +4,7 @@ export DATA_DIR=data/amazon_c4/inst/qwen-instruct
 export ROLLOUT_TP_SIZE=1
 export EXPERIMENT_NAME=matching-qwen2.5-0.5b-inst-ppo
 export VLLM_ATTENTION_BACKEND=XFORMERS
-export HF_HOME="~/.cache/huggingface"
+export HF_HOME="/home/rapids/.cache/huggingface"
 export CUDA_VISIBLE_DEVICES=0
 export PROJECT_NAME="adv-ml-project"
 
