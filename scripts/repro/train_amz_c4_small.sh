5906d62d9ed2c065e4b7d948ad1c514328c5ef03
Thomas Lilly <thomaslilly65@gmail.com>
2025-05-31 22:37:22 -0700

    Don't offload anything

--- scripts/repro/train_amz_c4_small.sh
+++ scripts/repro/train_amz_c4_small.sh
@@ -35,8 +35,8 @@ python -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.top_p=0.95 \
-    actor_rollout_ref.actor.fsdp_config.grad_offload=True \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size=2 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
