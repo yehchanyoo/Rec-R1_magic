03f49aee14a8ace78f5c2f7c9b06ac6d8e6903ad
Yeh Chan Yoo <13773473+yehchanyoo@users.noreply.github.com>
2025-06-02 08:24:13 -0700

    added progress messages

--- scripts/repro/amz_review_data_prep_part_1.sh
+++ scripts/repro/amz_review_data_prep_part_1.sh
@@ -2,8 +2,15 @@
 
 # Amazon Review data prep code
 # Part 1: Needs to be only run once
+# (Added echo lines to show progress due to long completion time)
+echo "[Step 1/5] Running process_amazon_2023.py..."
 python src/dataset/amazon_review/process_amazon_2023.py
+echo "[Step 2/5] Downloading All Beauty meta file..."
 wget https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/raw/meta_categories/meta_All_Beauty.jsonl.gz
+echo "[Step 3/5] Unzipping the downloaded All Beauty meta file..."
 gunzip meta_All_Beauty.jsonl.gz
+echo "[Step 4/5] Moving the file to appropriate directory..."
 mv meta_All_Beauty.jsonl data/amazon_review/raw
+echo "[Step 5/5] Running built_corpus.py..."
 python src/dataset/amazon_review/built_corpus.py
+echo "✅ Done! Now, run script/repro/amz_review_data_prep_part_2.sh."
