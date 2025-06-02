031ada6beb90fc4e1326cbf9db341010150611b6
Yeh Chan Yoo <13773473+yehchanyoo@users.noreply.github.com>
2025-06-02 00:49:28 -0700

    corrected relative filepath errors

--- scripts/repro/amz_review_data_prep_part_1.sh
+++ scripts/repro/amz_review_data_prep_part_1.sh
@@ -2,8 +2,8 @@
 
 # Amazon Review data prep code
 # Part 1: Needs to be only run once
-python /src/dataset/amazon_review/process_amazon_2023.py
+python src/dataset/amazon_review/process_amazon_2023.py
 wget https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/raw/meta_categories/meta_All_Beauty.jsonl.gz
 gunzip meta_All_Beauty.jsonl.gz
-mv meta_All_Beauty.jsonl ./data/amazon_review/raw
-python ./src/dataset/amazon_review/built_corpus.py
+mv meta_All_Beauty.jsonl data/amazon_review/raw
+python src/dataset/amazon_review/built_corpus.py
