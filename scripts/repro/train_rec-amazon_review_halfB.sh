03f2adabd0ba8af0e0beea2275b7dba61537435c
Yeh Chan Yoo <13773473+yehchanyoo@users.noreply.github.com>
2025-06-02 01:05:10 -0700

    updated train_rec-amazon_review_halfB.sh

--- scripts/repro/train_rec-amazon_review_halfB.sh
+++ scripts/repro/train_rec-amazon_review_halfB.sh
@@ -24,6 +24,7 @@ ray start --head --num-cpus=16 --num-gpus=1
 # Wait for Ray to initialize
 sleep 5
 
+## Changes made:
 # rollout.n = 12 -> 6
 # gpu_memory_utilization = 0.3 -> 0.7
 # ppo_mini_batch_size = 128 -> 64
@@ -54,5 +55,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.grad_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size=2 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
-    
+    ++actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    +trainer.num_gpus=1 \
+    +trainer.num_cpus=16 \
+    +actor_rollout_ref.model.tensor_parallel_size=1
+
+# Stop ray processes (only my own)
+ray stop
