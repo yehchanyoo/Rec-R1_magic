13a005dee5bc0b3d03e7bf39867e1841ddb601c8
Yeh Chan Yoo <13773473+yehchanyoo@users.noreply.github.com>
2025-06-02 19:41:30 -0700

    added further resetting of files

--- scripts/repro/amz_review_data_prep_part_2.sh
+++ scripts/repro/amz_review_data_prep_part_2.sh
@@ -3,10 +3,17 @@
 # Amazon Review data prep code
 # Part 2: Needs to be run every time we try to train
 
+# Make the parquets writable by user
+chmod u+w data/amazon_review/inst/*.parquet
+
 # Zero out the existing parquets
 for f in data/amazon_review/inst/*.parquet; do
   : > "$f"
 done
+
+# Delete past Lucene files before rebuilding Lucene database
+rm -rf database/amazon_review/All_Beauty/pyserini_index/
+rm -f database/amazon_review/All_Beauty/jsonl_docs/pyserini.jsonl
 
 # Run Amazon Beauty's version of subset_eq (changed to only do All Beauty)
 python src/dataset/amazon_review/inst/sparse/amazon_beauty.py
@@ -15,6 +22,9 @@ python src/dataset/amazon_review/inst/sparse/amazon_beauty.py
 python src/Lucene/amazon_review/1_convert_format.py
 bash src/Lucene/amazon_review/2_build_database.sh
 python src/Lucene/amazon_review/search.py
+
+# Make the parquets read-only
+chmod a-w data/amazon_review/inst/*.parquet
 
 # Train
 bash scripts/repro/train_rec-amazon_review_3b.sh
