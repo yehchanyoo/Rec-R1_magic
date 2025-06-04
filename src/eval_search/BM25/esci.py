0a16ba915178e3595cacf6b97bdd2034a4da2460
Justin Yang Chae <jchae3@klone-login03.hyak.local>
2025-06-04 11:10:11 -0700

    eval results

--- src/eval_search/BM25/esci.py
+++ src/eval_search/BM25/esci.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('./')
 
 from src.eval_search.utils import ndcg_at_k
-from src.Lucene.amazon_c4.search import PyseriniMultiFieldSearch
+from src.Lucene.esci.search import PyseriniMultiFieldSearch
 from src.eval_search.utils import extract_answer
 
 
