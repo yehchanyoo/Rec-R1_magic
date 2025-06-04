4b9ff35b3be7d0b40f33dbf09f2a5d9f6bbd2a02
Justin Yang Chae <jchae3@n3263.hyak.local>
2025-06-03 21:05:45 -0700

    added train scripts/slurm scripts and modified search.py for esci

---
+++ scripts/repro/train_esci_sparse.sh
@@ -0,0 +1,56 @@
+export N_GPUS=2
+export BASE_MODEL=Qwen/Qwen2.5-3B-Instruct
+export DATA_DIR=data/esci/inst/sparse/subset
+export ROLLOUT_TP_SIZE=1
+export DATE=$(date +%Y%m%d_%H%M%S)
+export EXPERIMENT_NAME="qwen2.5-3b-inst-ppo-${SLURM_JOB_NAME}-${DATE}"
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export HF_HOME="/home/rapids/Rec-R1_magic/.cache/huggingface"
+export CUDA_VISIBLE_DEVICES=0,1
+export PROJECT_NAME="adv-ml-project"
+
+# Data Prep
+python src/Lucene/esci/1_convert_format.py
+bash src/Lucene/esci/2_build_database.sh
+
+# Trainer
+python -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=$DATA_DIR/train.parquet \
+    data.val_files=$DATA_DIR/val.parquet \
+    data.train_batch_size=32 \
+    data.val_batch_size=32 \
+    data.max_prompt_length=256 \
+    data.max_response_length=512 \
+    actor_rollout_ref.model.path=$BASE_MODEL \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.strategy=fsdp \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=2 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.rollout.temperature=0.6 \
+    actor_rollout_ref.rollout.top_p=0.95 \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.n=2 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=2 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=False \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.logger=['wandb'] \
+    +trainer.val_before_train=False \
+    trainer.default_hdfs_dir=null \
+    trainer.n_gpus_per_node=$N_GPUS \
+    trainer.nnodes=1 \
+    trainer.save_freq=50 \
+    trainer.test_freq=10 \
+    trainer.project_name=$PROJECT_NAME \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.total_epochs=1 2>&1 | tee logs/esci_sparse_3b_grpo-verl_2gpu_${DATE}.log
